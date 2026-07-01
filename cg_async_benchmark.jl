5712044d9e10ded63a4397f8774eb25bf06470d8
Chiang D W <pwy@login-excl.ornl.gov>
2026-07-01 09:44:36 -0400

    separate cg and cg_async scripts, write slurm script

---
+++ cg_async_benchmark.jl
@@ -0,0 +1,97 @@
+include("setup.jl")
+
+using BenchmarkTools
+using DelimitedFiles
+using Dates
+using Statistics
+
+suite = BenchmarkGroup()
+
+SIZE = 200_000_000
+
+function matvecmul(i, a1, a2, a3, x, y, SIZE)
+        if i == 1
+            y[i] = a2[i] * x[i] + a1[i] * x[i + 1]
+        elseif i == SIZE
+            y[i] = a3[i] * x[i - 1] + a2[i] * x[i]
+        elseif i > 1 && i < SIZE
+            y[i] = a3[i] * x[i - 1] + a2[i] * +x[i] + a1[i] * +x[i + 1]
+        end
+    end
+
+suite["cg_async"] = 
+
+let 
+        a0 = JACC.Async.ones(2, SIZE)
+        a1 = JACC.Async.ones(2, SIZE)
+        a2 = JACC.Async.ones(2, SIZE)
+        r = JACC.Async.ones(1, SIZE)
+        p = JACC.Async.ones(2, SIZE)
+        s1 = JACC.Async.zeros(2, SIZE)
+        s2 = JACC.Async.zeros(1, SIZE)
+        x = JACC.Async.zeros(2, SIZE)
+        r_old = JACC.Async.zeros(2, SIZE)
+        r_aux = JACC.Async.zeros(2, SIZE)
+	CUDA.device!(1)
+        a1 = a1 * 4
+	CUDA.device!(0)
+        r = r * 0.5
+	CUDA.device!(1)
+        p = p * 0.5
+        cond = 1.0
+
+@benchmarkable begin 
+    while cond[1, 1] >= 1e-14
+        copyto!($r, $r_old)
+
+        JACC.Async.parallel_for(2, $SIZE, matvecmul, $a0, $a1, $a2, $p, $s1, $SIZE)
+
+        alpha1 = JACC.Async.parallel_reduce(2, $SIZE, dot, $p, $s1)
+        alpha0 = JACC.Async.parallel_reduce(1, $SIZE, dot, $r, $r)
+        JACC.Async.synchronize()
+
+        alpha = JACC.to_host(alpha0)[] / JACC.to_host(alpha1)[]
+        negative_alpha = alpha * -1.0
+
+        copyto!($s2, $s1)
+        JACC.Async.parallel_for(2, $SIZE, axpy, alpha, $x, $p)
+        JACC.Async.parallel_for(1, $SIZE, axpy, negative_alpha, $r, $s2)
+        JACC.Async.synchronize()
+
+        beta1 = JACC.Async.parallel_reduce(2, $SIZE, dot, $r_old, $r_old)
+        beta0 = JACC.Async.parallel_reduce(1, $SIZE, dot, $r, $r)
+        JACC.Async.synchronize()
+        beta = JACC.to_host(beta0)[] / JACC.to_host(beta1)[]
+
+        copyto!($r, $r_aux)
+
+        JACC.Async.parallel_for(2, $SIZE, axpy, beta, $r_aux, $p)
+        ccond = JACC.Async.parallel_reduce(1, $SIZE, dot, $r, $r)
+        JACC.Async.synchronize()
+        cond = JACC.to_host(ccond)[]
+
+        copyto!($p, $r_aux)
+    end
+end seconds = 300 samples = 100 evals = 1 gcsample = true setup = (CUDA.device!(1); $x .= 0.0; CUDA.device!(0); $r .= 0.5; $CUDA.device!(1); $p .= 0.5; cond = 1.0)
+end
+
+results = run(suite)
+
+#store times as a single, comma-separated row 
+writedlm("times.csv", permutedims(results["cg_async"].times), ',')
+
+#store metrics in a separate file
+open("metrics.txt", "w") do file
+	println(file, "SIZE: $SIZE")
+
+	println(file, "samples: $(length(results["cg_async"].times))")
+
+	println(file, "mean_gc: $(mean(results["cg_async"].gctimes)/1e9)")
+
+	println(file, "min: $(minimum(results["cg_async"].times)/1e9)")
+	println(file, "max: $(maximum(results["cg_async"].times)/1e9)")
+	println(file, "median: $(median(results["cg_async"].times)/1e9)")
+	println(file, "mean: $(mean(results["cg_async"].times)/1e9)")
+
+	println(file, "variation: $(std(results["cg_async"].times)/1e9)")
+end
--- cg_benchmark.jl
+++ cg_benchmark.jl
@@ -3,8 +3,11 @@ include("setup.jl")
 using BenchmarkTools
 using DelimitedFiles
 using Dates
+using Statistics
 
 suite = BenchmarkGroup()
+
+SIZE = 1_000_000_000
 
 function matvecmul(i, a1, a2, a3, x, y, SIZE)
         if i == 1
@@ -16,9 +19,8 @@ function matvecmul(i, a1, a2, a3, x, y, SIZE)
         end
     end
 
-#= suite["cg"] = let 
+suite["cg"] = let 
 
-	SIZE = 800_000_000
     a0 = JACC.ones(SIZE)
     a1 = JACC.ones(SIZE)
     a2 = JACC.ones(SIZE)
@@ -61,69 +63,25 @@ function matvecmul(i, a1, a2, a3, x, y, SIZE)
 	copyto!($p, $r_aux) #= used to be p = copy(r_aux) but 
         wrapping the loop in @benchmark changes the scope to be local =#
     end 
-end evals = 1 samples = 1 gcsample = true setup = ($x .= 0.0; $r .= 0.5; $p .= 0.5; cond = 1.0)
+end seconds = 300 samples = 100 evals = 1 gcsample = true setup = ($x .= 0.0; $r .= 0.5; $p .= 0.5; cond = 1.0)
 end
-=#
 
-suite["cg_async"] = 
-
-let 
-
-    SIZE = 1_200_000_000
-        a0 = JACC.Async.ones(2, SIZE)
-        a1 = JACC.Async.ones(2, SIZE)
-        a2 = JACC.Async.ones(2, SIZE)
-        r = JACC.Async.ones(1, SIZE)
-        p = JACC.Async.ones(2, SIZE)
-        s1 = JACC.Async.zeros(2, SIZE)
-        s2 = JACC.Async.zeros(1, SIZE)
-        x = JACC.Async.zeros(2, SIZE)
-        r_old = JACC.Async.zeros(2, SIZE)
-        r_aux = JACC.Async.zeros(2, SIZE)
-	CUDA.device!(1)
-        a1 = a1 * 4
-	CUDA.device!(0)
-        r = r * 0.5
-	CUDA.device!(1)
-        p = p * 0.5
-        cond = 1.0
-
-@benchmarkable begin 
-    while cond[1, 1] >= 1e-14
-        copyto!($r, $r_old)
-
-        JACC.Async.parallel_for(2, $SIZE, matvecmul, $a0, $a1, $a2, $p, $s1, $SIZE)
-
-        alpha1 = JACC.Async.parallel_reduce(2, $SIZE, dot, $p, $s1)
-        alpha0 = JACC.Async.parallel_reduce(1, $SIZE, dot, $r, $r)
-        JACC.Async.synchronize()
-
-        alpha = JACC.to_host(alpha0)[] / JACC.to_host(alpha1)[]
-        negative_alpha = alpha * -1.0
-
-        copyto!($s2, $s1)
-        JACC.Async.parallel_for(2, $SIZE, axpy, alpha, $x, $p)
-        JACC.Async.parallel_for(1, $SIZE, axpy, negative_alpha, $r, $s2)
-        JACC.Async.synchronize()
-
-        beta1 = JACC.Async.parallel_reduce(2, $SIZE, dot, $r_old, $r_old)
-        beta0 = JACC.Async.parallel_reduce(1, $SIZE, dot, $r, $r)
-        JACC.Async.synchronize()
-        beta = JACC.to_host(beta0)[] / JACC.to_host(beta1)[]
-
-        copyto!($r, $r_aux)
-
-        JACC.Async.parallel_for(2, $SIZE, axpy, beta, $r_aux, $p)
-        ccond = JACC.Async.parallel_reduce(1, $SIZE, dot, $r, $r)
-        JACC.Async.synchronize()
-        cond = JACC.to_host(ccond)[]
-
-        copyto!($p, $r_aux)
-    end
-end seconds = 200 samples = 100 evals = 1 gcsample = true setup = (CUDA.device!(1); $x .= 0.0; CUDA.device!(0); $r .= 0.5; $CUDA.device!(1); $p .= 0.5; cond = 1.0)
-end
 
 results = run(suite)
 
-writedlm("results $(now())", ["cg_async"].times, ',')
+writedlm("times.csv", permutedims(results["cg"].times), ',')
 
+open("metrics.txt", "w") do file
+	println(file, "SIZE: $SIZE")
+
+	println(file, "samples: $(length(results["cg"].times))")
+
+	println(file, "mean_gc: $(mean(results["cg"].gctimes)/1e9)")
+
+	println(file, "min: $(minimum(results["cg"].times)/1e9)")
+	println(file, "max: $(maximum(results["cg"].times)/1e9)")
+	println(file, "median: $(median(results["cg"].times)/1e9)")
+	println(file, "mean: $(mean(results["cg"].times)/1e9)")
+
+	println(file, "variation: $(std(results["cg"].times)/1e9)")
+end
