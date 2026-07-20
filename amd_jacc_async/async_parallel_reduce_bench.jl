f55f0d1217d1383c528c5fb2f2e76190883b525a
Doris Chiang <dorischiang@login07.frontier.olcf.ornl.gov>
2026-07-20 15:08:32 -0400

    add amd bench scripts

--- amd_jacc_async/async_parallel_reduce_bench.jl
+++ amd_jacc_async/async_parallel_reduce_bench.jl
@@ -17,7 +17,7 @@ r  = JACC.Async.ones(1, SIZE)
 p  = JACC.Async.ones(2, SIZE)
 s1 = JACC.Async.zeros(2, SIZE)
 r  = r * 0.5
-AMDGPU.device(devices[2])
+AMDGPU.device!(devices[2])
 p  = p * 0.5
 
 #warmup run
